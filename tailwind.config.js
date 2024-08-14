b52c31d26c88e4ea0ff83b0e04b5bbf10818576f
samimOsman754 <msosman754@gmail.com>
2024-08-15 00:58:57 +0600

    level2 assignment1

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans:['Inter', 'sans-serif'],
+      },
+      container: {
+        center: true,
+        padding: '1.5rem',
+      },
+    },
+  },
+  plugins: [],
+}
+
